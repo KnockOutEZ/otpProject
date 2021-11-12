32414a353c3ca994fae14bdea8d6475de7806898
KnockOutEZ <ktowhid20@gmail.com>
2021-11-12 23:14:40 +0600

    dashboard layout complete

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -267,7 +267,7 @@
       <!-- Mobile sidebar -->
       <!-- Backdrop -->
       <div
-        v-show="isSideMenuOpen"
+        v-if="isSideMenuOpen"
         x-transition:enter="transition ease-in-out duration-150"
         x-transition:enter-start="opacity-0"
         x-transition:enter-end="opacity-100"
@@ -281,7 +281,7 @@
       v-click-away="closeSideMenu"
         @keydown.escape="closeSideMenu"
         class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden"
-        v-show="isSideMenuOpen"
+        v-if="isSideMenuOpen"
         x-transition:enter="transition ease-in-out duration-150"
         x-transition:enter-start="opacity-0 transform -translate-x-20"
         x-transition:enter-end="opacity-100"
